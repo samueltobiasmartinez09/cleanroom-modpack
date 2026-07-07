ee1cd852f637617fe1c5a29fb46a7101912fb381
samurey09 <test@test.com>
2026-07-07 10:27:33 -0300

    switch-to-optifine: fix OptiFine link (optifine.net downloadx expirado -> GitHub Archive)

--- minecraft/switch-to-optifine.sh
+++ minecraft/switch-to-optifine.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit 1
 echo "Downloading OptiFine..."
-curl -sL -o "mods/preview_OptiFine_1.12.2_HD_U_G6_pre1.jar" "https://optifine.net/downloadx?f=preview_OptiFine_1.12.2_HD_U_G6_pre1.jar&x=58c677dbe2bc066f4d7c69536e233155"
+curl -sL -o "mods/preview_OptiFine_1.12.2_HD_U_G6_pre1.jar" "https://raw.githubusercontent.com/SynArchive/OptiFine-Archive/main/1.12.2/preview_OptiFine_1.12.2_HD_U_G6_pre1.jar"
 curl -sL -o "mods/OptiNotFine-1.0.jar" "https://cdn.modrinth.com/data/TqfSjjiB/versions/mlcZgOCz/OptiNotFine-1.0.jar"
 echo "Disabling Celeritas..."
 for f in celeritas-forge-mc12.2-*.jar celeritasdynamiclights-*.jar celeritasextra-*.jar celeritasleafculling-*.jar; do
