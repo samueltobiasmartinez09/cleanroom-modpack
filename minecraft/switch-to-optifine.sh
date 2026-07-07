6b4c0ed323adb4e7f38a5bf98b26bdc5a98505e8
samurey09 <test@test.com>
2026-07-07 10:24:36 -0300

    switch-to-optifine: fix OptiNotFine link (CurseForge 403 -> Modrinth)

--- minecraft/switch-to-optifine.sh
+++ minecraft/switch-to-optifine.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit 1
 echo "Downloading OptiFine..."
-curl -sL -o "mods/preview_OptiFine_1.12.2_HD_U_G6_pre1.jar" "https://optifine.net/downloadx?f=preview_OptiFine_1.12.2_HD_U_G6_pre1.jar&x=57ce0cdd0c77666d893a09f5c8971bcd"
-curl -sL -o "mods/OptiNotFine-1.0.jar" "https://media.forgecdn.net/files/7438/360/OptiNotFine-1.0.jar"
+curl -sL -o "mods/preview_OptiFine_1.12.2_HD_U_G6_pre1.jar" "https://optifine.net/downloadx?f=preview_OptiFine_1.12.2_HD_U_G6_pre1.jar&x=58c677dbe2bc066f4d7c69536e233155"
+curl -sL -o "mods/OptiNotFine-1.0.jar" "https://cdn.modrinth.com/data/TqfSjjiB/versions/mlcZgOCz/OptiNotFine-1.0.jar"
 echo "Disabling Celeritas..."
 for f in celeritas-forge-mc12.2-*.jar celeritasdynamiclights-*.jar celeritasextra-*.jar celeritasleafculling-*.jar; do
     [ -f "mods/$f" ] && mv "mods/$f" "mods/$f.disabled"
