6b49fab99cbf3d5d3beee9f03a916702bea51ff0
samurey09 <test@test.com>
2026-07-07 08:15:11 -0300

    Fix JavaPath resolution: add wrapper scripts at instance root + minecraft dir
    - instance.cfg: JavaPath=./java-bootstrap (instead of ./minecraft/jre25/...)
    - Add java-bootstrap shell wrapper (root + minecraft/) that searches for ELF binary
    - Add java-bootstrap.bat wrapper (root + minecraft/) for Windows
    - build-zips.sh: filter platform-specific wrappers, update JavaPath for Windows
    - .gitignore: track wrappers, exclude mod jars / resource zips
    - .packwizignore: exclude wrappers from packwiz

---
+++ minecraft/java-bootstrap.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
+set "SCRIPT_DIR=%~dp0"
+if exist "%SCRIPT_DIR%..\minecraft\jre25\java-bootstrap.exe" (
+    "%SCRIPT_DIR%..\minecraft\jre25\java-bootstrap.exe" %*
+    exit /b !ERRORLEVEL!
+)
+if exist "%SCRIPT_DIR%jre25\java-bootstrap.exe" (
+    "%SCRIPT_DIR%jre25\java-bootstrap.exe" %*
+    exit /b !ERRORLEVEL!
+)
+echo java-bootstrap: binary not found
+exit /b 1
